8b60249646f5b46cc1f991244a99e88d2c5fc4bc
Peerachet Porkaew <peerachet.porkaew@gmail.com>
2024-10-08 13:16:27 +0700

    update

---
+++ queueflow/main_ollama.py
@@ -0,0 +1,33 @@
+from openai import OpenAI
+from pydantic import BaseModel, Field
+from typing import List
+
+import instructor
+
+
+class Character(BaseModel):
+    name: str
+    age: int
+    fact: List[str] = Field(..., description="A list of facts about the character")
+
+
+# enables `response_model` in create call
+client = instructor.from_openai(
+    OpenAI(
+        base_url="http://localhost:11434/v1",
+        api_key="ollama",  # required, but unused
+    ),
+    mode=instructor.Mode.JSON,
+)
+
+resp = client.chat.completions.create(
+    model="gemma:2b",
+    messages=[
+        {
+            "role": "user",
+            "content": "Tell me about the Harry Potter",
+        }
+    ],
+    response_model=Character,
+)
+print(resp.model_dump_json(indent=2))
